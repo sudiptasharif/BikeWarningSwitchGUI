dd6a0583a3edfa5f509d52ee773e5cc158aa09ee
Sudipta Sharif <sudiptasharif@gmail.com>
2021-12-22 22:09:33 -0600

    SUtils: copied needed code from the terminal version

--- src/switchutility/SUtils.java
+++ src/switchutility/SUtils.java
@@ -4,10 +4,35 @@
  */
 package switchutility;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  *
  * @author sudiptasharif
  */
 public class SUtils {
+    public static final String DATE_FORMAT_USER_FRIENDLY = "yyyy-MM-dd hh:mm a";
+    public static final String DATE_FORMAT_YYYY_MM_DD = "yyyy-MM-dd";
+    public static final String DATE_FORMAT_HH_MM_SS_MSSS = "hh:mm:ss.SSS";
+    public static final long DEFAULT_MILLISECOND = 0;
+    public static final String CONNECTION_SUCCESS = "Connection SUCCESSFUL\n";
+    public static final String CONNECTION_FAILURE = "Connection FAILED\n\nMake sure server is on\nIn App Press Button: START LISTENING\n";
+    public static final int SERVER_ON = 1;
+    public static final int SERVER_OFF = 0;
+    public static final int INVALID_WARNING_CODE = 0;
     
+    public static String formatDate(Calendar calendar, String dateFormatPattern){
+        String formattedDate = "";
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormatPattern);
+        Date date = calendar.getTime();
+        formattedDate = formatter.format(date);
+        return formattedDate;
+    }
+
+    public  static String formatDate(long timeInMillis, String dateFormatPattern) {
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormatPattern);
+        return formatter.format(new Date(timeInMillis));
+    }
 }
